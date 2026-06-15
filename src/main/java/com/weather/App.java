ecda63c16fb42d3c426abc34ffa76f2ab7630ad1
Vijjwal30 <vijjwal30@gmail.com>
2026-06-15 12:08:14 +0530

    Changed application background

--- src/main/java/com/weather/App.java
+++ src/main/java/com/weather/App.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Stop;
+import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -23,14 +24,19 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         Label label = new Label("JavaFX 26 + Gluon Mobile");
+
+        label.setFont(new Font(20));
+        label.setTextFill(Color.DARKCYAN);
+
         StackPane root = new StackPane(label);
         Background background = new Background(
                 new BackgroundFill(
                         GradientBuilder.newBuilder()
-                                .setAngle(30)
+                                .setAngle(80)
                                 .addStops(
-                                        new Stop(0.0,Color.rgb(233, 233, 245, 1.0)),
-                                        new Stop(0.4,Color.rgb(235, 235, 240, 1.0)),
+                                        new Stop(0.0,Color.rgb(142, 216, 245, 1.0)),
+                                        new Stop(0.3,Color.rgb(235,235,240,1.0)),
+                                        new Stop(0.7,Color.rgb(235, 235, 240, 1.0)),
                                         new Stop(1.0,Color.rgb(142, 216, 245, 1.0))
                                 )
                                 .build(), null,null
